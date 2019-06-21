735c0e1fe27058894049ea4f49c790983953b513
SH-H-B <shiva19173@gmail.com>
2019-06-21 10:46:26 +0100

    add material UI to login

--- src/components/Login/LoginBox.jsx
+++ src/components/Login/LoginBox.jsx
@@ -1,3 +1,36 @@
 import React from "react";
+import { TextField } from "@material-ui/core";
 
-class LoginBox extends React.Component {}
+class LoginBox extends React.Component {
+  state = {
+    userIput: "",
+    err: null
+  };
+
+  storeUserInput = event => {
+    this.setState({ userInput: event.target.value });
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <FormGroup className={classes.form}>
+          <div>
+            <TextField
+              id="login"
+              label={this.state.err ? "Invalid user !" : "Username"}
+              onChange={this.storeUserInput}
+              helperText="fraserkemp"
+              variant="outlined"
+            />
+          </div>
+          <Button type="submit">
+            <Typography>Login</Typography>
+          </Button>
+        </FormGroup>
+      </form>
+    );
+  }
+}
+
+export default LoginBox;
