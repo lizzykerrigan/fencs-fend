1d0f00da4d98f06b00df3eeaf2fadf2d73337241
Elizabeth Kerrigan <lizzykerrigan@outlook.com>
2019-06-21 10:04:31 +0100

    add home page with images and titles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import CategoriesPage from "./components/CategoriesPage/CategoriesPage";
 import { Router } from "@reach/router";
 import AboutPage from "./components/AboutPage/AboutPage";
+import HomePage from "./components/HomePage/HomePage";
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
       <div>
         <Header />
         <Router>
+          <HomePage path="/" />
           <CategoriesPage path="/categories" />
           <AboutPage path="/about_us" />
         </Router>
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import HomePageCard from "../CardComponents/HomePageCard";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const styles = theme => ({
+  root: {
+    marginTop: "20px",
+    flexGrow: 1
+  }
+});
+
+class HomePage extends React.Component {
+  //const [spacing] = React.useState(3);
+
+  componentDidMount() { }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            <Query
+              query={gql`
+                {
+                  images {
+                    title
+                    display_name
+                    thumbnail_url
+                    likes
+                  }
+                }
+              `}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error :(</p>;
+
+                return data.images.map(image => (
+                  <HomePageCard image={image} />
+                ));
+              }}
+            </Query>
+            {/* {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(value => (
+              <Grid key={value} item>
+                <CategoryCard />
+              </Grid>
+            ))} */}
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(HomePage);
