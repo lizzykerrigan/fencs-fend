991f62ee9f5b5b25e8fa6dae7218a93b5033ca21
SH-H-B <shiva19173@gmail.com>
2019-06-25 16:33:23 +0100

    minor changes to the Link

--- src/components/CardComponents/HomePageCard.jsx
+++ src/components/CardComponents/HomePageCard.jsx
@@ -9,6 +9,10 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "@reach/router";
 
+const AdapterLink = React.forwardRef((props, ref) => (
+  <Link innerRef={ref} {...props} />
+));
+
 const useStyles = makeStyles({
   header: {
     textTransform: "capitalize"
@@ -27,34 +31,36 @@ export default function HomePageCard(props) {
   const classes = useStyles();
 
   return (
-    <Link to={`/images/${props.image.image_id}`}>
-      <Card className={classes.card}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={props.image.thumbnail_url}
-            title={props.image.display_name}
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="h2"
-              className={classes.header}
-            >
-              {props.image.title}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {props.image.likes} likes
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            View Image
-          </Button>
-        </CardActions>
-      </Card>
-    </Link>
+    <Card
+      className={classes.card}
+      component={AdapterLink}
+      to={`/images/${props.image.image_id}`}
+    >
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.image.thumbnail_url}
+          title={props.image.display_name}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h2"
+            className={classes.header}
+          >
+            {props.image.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.image.likes} likes
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          View Image
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
